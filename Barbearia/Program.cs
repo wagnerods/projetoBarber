9a3caed1f2404b61cbcec6ce92012abe945c7c81
DIOGO ARAÚJO DOS SANTOS <082180013@ftt.cefsa.edu.br>
2020-10-25 12:20:05 -0300

    Diogo pegou o projeto

--- Barbearia/Program.cs
+++ Barbearia/Program.cs
@@ -14,7 +14,7 @@ namespace Barbearia
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run(); //testediogo
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
